45f6d8cd9d0a4bd9b1b459b62dcc797692fa3daf
Alex Zimin <alex.zimin@hotmail.com>
2022-06-07 09:33:19 +0300

    initial commit, add nft smart contract

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,3 +1,5 @@
+const { ethers } = require("hardhat");
+
 async function main() {
 
   const [deployer] = await ethers.getSigners();
@@ -5,17 +7,17 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // deploy contracts here:
+  const NFT = await ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy();
   
-  
+  console.log("NFT contract address: ", nft.address);
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles();
+  saveFrontendFiles(nft, "NFT");
 }
 
 function saveFrontendFiles(contract, name) {
   const fs = require("fs");
   const contractsDir = __dirname + "/../../frontend/contractsData";
-
   if (!fs.existsSync(contractsDir)) {
     fs.mkdirSync(contractsDir);
   }
